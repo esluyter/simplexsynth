1685500d1edfd9694875c600756710e428d41f03
Eric Sluyter <wondersluyter@gmail.com>
2020-12-22 15:18:58 -0500

    initial poc

--- plugins/SimplexSynth/SimplexSynth.cpp
+++ plugins/SimplexSynth/SimplexSynth.cpp
@@ -3,6 +3,7 @@
 
 #include "SC_PlugIn.hpp"
 #include "SimplexSynth.hpp"
+#include "SimplexNoise.h"
 
 static InterfaceTable* ft;
 
@@ -14,14 +15,24 @@ SimplexSynth::SimplexSynth() {
 }
 
 void SimplexSynth::next(int nSamples) {
-    const float* input = in(0);
-    const float* gain = in(0);
+    const float* x = in(0);
+    const float* y = in(1);
+    float oct = in0(2);
     float* outbuf = out(0);
 
-    // simple gain function
     for (int i = 0; i < nSamples; ++i) {
-        outbuf[i] = input[i] * gain[i];
+      float xtemp = x[i];
+      float ytemp = y[i];
+      float weight = 1.0;
+      outbuf[i] = SimplexNoise::noise(xtemp, ytemp);
+      for (int n = 1; n < oct; ++n) {
+        xtemp *= 2;
+        ytemp *= 2;
+        weight /= 2;
+        outbuf[i] = outbuf[i] + (SimplexNoise::noise(xtemp, ytemp) * weight);
+      }
     }
+
 }
 
 } // namespace SimplexSynth
--- plugins/SimplexSynth/SimplexSynth.sc
+++ plugins/SimplexSynth/SimplexSynth.sc
@@ -1,7 +1,17 @@
 SimplexSynth : UGen {
-	*ar { |input, gain|
-		/* TODO */ ^this.multiNew('audio', input, gain);
+	*ar { |freq=100, offsetX=0, offsetY=0, freqScale=2, oct=3, syncSig=0|
+		var phase = Phasor.ar(syncSig, SampleDur.ir * freq) * 2pi;
+	  ^this.ar2(
+	    SinOsc.ar(0, (phase + (pi/2))) * freqScale + offsetX,
+	    SinOsc.ar(0, phase) * freqScale + offsetY,
+	    oct
+	  );
 	}
+	
+	*ar2 { |x=0, y=0, oct=3|
+		^this.multiNew('audio', x, y, max(1, oct));
+	}
+
 	checkInputs {
 		/* TODO */
 		^this.checkValidInputs;
